bced343efb840e7f12588610a9a8a9037e065092
vitorfguerra <86574114+vitorfguerra@users.noreply.github.com>
2021-06-27 17:46:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+/* abre e fecha o menu quando clicar no link*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*quando clicar em um item, fechar o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*mudar o header da página quando der scroll*/
+
+function chageHeaderWhenScroll() {
+  const headr = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+/* Testimonials Slider */
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* Scrollreveal - mostrar elemoentos conforme rola a página*/
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .image, #home .text,
+#about .image, #about .text,
+#services .header, #services .card,
+#testimonials .header, #testimonials .testimonials,
+#contact .text, #contact .links,
+footer .brand, footer .social
+`,
+  { interval: 100 }
+)
+
+/*Botção voltar ao topo*/
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (this.window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  chageHeaderWhenScroll()
+  backToTop()
+})
